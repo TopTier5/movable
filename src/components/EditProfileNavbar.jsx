c58aed58e394e29d750bd160710a44c2d7cbc41e
vonette-ab <vonwrkspc@gmail.com>
2025-07-23 13:49:15 +0000

    editprofile

--- src/components/EditProfileNavbar.jsx
+++ src/components/EditProfileNavbar.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router"
+import { ChevronLeft } from "lucide-react"
+
+
+
+
 export default function EditProfileNavbar() {
     return(
         <div>
-            
+           <nav className='flex w-full border border-white shadow-md'>
+            <div className='ml-50 flex mt-7'>
+        <Link to={"/userpage"} className='flex mr-7 mb-6 mt-2 cursor-pointer hover:text-blue-500'><ChevronLeft className="w-6 h-6 text-gray-700 mr-3 hover:text-blue-500" />
+        <span className='mr-2'>Back to Dashboard</span> 
+        </Link> 
+      <h1 className="text-2xl font-bold mb-7">Edit Profile</h1>
+      </div>
+        </nav>
+    
         </div>
     )
 }
--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,35 +1,36 @@
 import { Link } from "react-router";
+import EditProfileNavbar from "../components/EditProfileNavbar";
 
 export default function EditProfile() {
-  
+
     return (
         <>
-            
+            <EditProfileNavbar />
 
 
             <section className="bg-[#F9FAFB] py-12">
                 <div className="w-[60%] max-w-3xl mx-auto bg-[#FFFFFF]  border border-[#D8E1E9] rounded-2xl shadow-lg p-8">
                     <form className="flex flex-col space-y-6 ">
-                        <h1 className="text-black text-4xl text-left font-bold "> Personal Information</h1>
+                        <h1 className="text-black text-2xl text-left font-bold "> Personal Information</h1>
 
                         <div className="grid grid-cols-2 gap-4 justify-between space-x-4">
                             <div className="w-20%">
-                                <label className="block mb-2 font-semi-bold text-black">Full Name</label>
+                                <label className="block mb-2 font-bold text-black">Full Name</label>
                                 <input
                                     type="text"
                                     name="fullName"
                                     className="w-full border border-[#D8E1E9] rounded px-4 py-2"
-                                    >
+                                >
                                 </input>
                             </div>
 
                             <div className="w-20%">
-                                <label className="block mb-2 font-semi-bold text-black">Email Address</label>
+                                <label className="block mb-2 font-bold text-black">Email Address</label>
                                 <input
                                     type="text"
                                     name="emailAddress"
                                     className="w-full border border-[#D8E1E9] rounded px-4 py-2"
-                                  
+
 
                                 />
                             </div>
@@ -40,7 +41,7 @@ export default function EditProfile() {
                                     type="tel"
                                     name="phoneNumber"
                                     className="w-full border border-[#D8E1E9] rounded px-4 py-2"
-                                    
+
                                 />
                             </div>
 
@@ -52,7 +53,7 @@ export default function EditProfile() {
                                     type="text"
                                     name="address"
                                     className="w-full border border-[#D8E1E9] rounded px-4 py-2"
-                                     />
+                                />
                             </div>
 
                         </div>
@@ -64,23 +65,23 @@ export default function EditProfile() {
 
 
             <section className="bg-[#F9FAFB] py-12">
-                <div className="w-[60%] max-w-3xl mx-auto bg-[#FFFFFF]  border border-[#D8E1E9] rounded-2xl shadow-lg p-6"></div>
+                <div className="w-[60%] max-w-3xl mx-auto bg-[#FFFFFF]  border border-[#D8E1E9] rounded-2xl shadow-lg p-8">
                 <form className="flex flex-col space-y-6 ">
-                    <h1 className="text-black text-4xl text-left font-bold "> Emergency Contact</h1>
+                    <h1 className="text-black text-2xl text-left font-bold "> Emergency Contact</h1>
 
                     <div className="grid grid-cols-2 gap-4 justify-between space-x-4">
                         <div className="w-20%">
-                            <label className="block mb-2 font-semi-bold text-black">Contact Name</label>
+                            <label className="block mb-2 font-bold text-black">Contact Name</label>
                             <input
                                 type="text"
                                 name="contactName"
                                 className="w-full border border-[#D8E1E9] rounded px-4 py-2"
-                                >
+                            >
                             </input>
                         </div>
 
                         <div className="w-20%">
-                            <label className="block mb-2 font-semi-bold text-black">Contact Number</label>
+                            <label className="block mb-2 font-bold text-black">Contact Number</label>
                             <input
                                 type="tel"
                                 name="contactNumber"
@@ -89,13 +90,15 @@ export default function EditProfile() {
                         </div>
                     </div>
                 </form>
-
+                </div>
             </section>
 
-            <div className="flex justify-end">
+
+            
+            <div className="flex justify-center ml-148 ">
                 <button
                     type="submit"
-                    className="bg-blue-600 fixed bottom-4 right-4 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors"
+                    className="bg-blue-600  text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors"
                 >
                     💾   Save Changes
                 </button>
